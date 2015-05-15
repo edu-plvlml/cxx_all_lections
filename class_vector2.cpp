c5c74995c0c8bce99dcce65bc0ba3d37f5730b5a
Pavel Matcula <dev.plvlml@gmail.com>
2015-05-15 22:48:47 +0200

    Изменен порядок примеров и комментариев

--- class_vector2.cpp
+++ class_vector2.cpp
@@ -10,14 +10,6 @@ class Vector2;
 // Определение объектов класса Vector2
 Vector2 v2, *pv2 = new Vector2; // теперь можно
 
-// Определение конструктора с параметрами
-Vector2::Vector2(int x /* = 1 */, int y /* = 1 */)
-    // Список инициализации
-    : x_(x), y_(y) { // правильно: конструктор инициализирует поля
-  // Присваивание значений полям
-  // x_ = x; y_ = y; // плохо: поля уже инициализированы
-}
-
 // Определение метода класса Vector2
 int Vector2::get_y(/* const Vector2& vector_from */) const {
   // const Vector2* this = &vector_from;
@@ -28,4 +20,12 @@ int Vector2::get_y(/* const Vector2& vector_from */) const {
 void Vector2::set_y(/* Vector2& vector_to, */ int y) {
   // Vector2* this = &vector_to;
   this->y_ = y;
+}
+
+// Определение конструктора с параметрами
+Vector2::Vector2(int x /* = 0 */, int y /* = 0 */)
+    // Список инициализации
+    : x_(x), y_(y) { // правильно: конструктор инициализирует поля
+  // Присваивание значений полям
+  // x_ = x; y_ = y; // плохо: поля уже инициализированы
 }
